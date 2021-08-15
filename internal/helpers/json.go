5ff0d77084346ee5618221944cdb047803b76ded
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-08-16 04:01:10 +1000

    use new slice again

--- internal/helpers/json.go
+++ internal/helpers/json.go
@@ -34,7 +34,7 @@ func CreateJSON(tgs []PromTargetFile, folder string) error {
 }
 
 func CreateFileName(folder string, targets []string) string {
-	noSchemeTargets := targets[:0]
+	var noSchemeTargets []string
 	for i := range targets {
 		noSchemeTargets = append(noSchemeTargets, strings.TrimPrefix(targets[i], SCHEME))
 	}
