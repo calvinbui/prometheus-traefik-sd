9f3acd4bb3fab31fb5dba6b818a0e404eab29284
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-08-16 01:41:54 +1000

    work on the slice

--- internal/helpers/grace.go
+++ internal/helpers/grace.go
@@ -24,9 +24,9 @@ func DeleteOldTargets(tgs []PromTargetFile, folder string, gFiles []GraceFile, g
 existingFiles:
 	for _, f := range existingFiles {
 		if !targetExists(tgs, f) {
-			for _, g := range gFiles {
+			for i, g := range gFiles {
 				if g.FilePath == f {
-					g.Count = g.Count + 1
+					gFiles[i].Count++
 					logger.Debug(fmt.Sprintf("%s grace period count is now %v", g.FilePath, g.Count))
 					continue existingFiles
 				}
@@ -38,16 +38,19 @@ existingFiles:
 	}
 
 	logger.Debug(fmt.Sprintf("Grace period count is set to %v", gracePeriod))
+	var newGFiles []GraceFile
 	for _, g := range gFiles {
 		if g.Count >= gracePeriod {
 			logger.Info(fmt.Sprintf("Deleting %s as grace period has exceeded (%v)", g.FilePath, g.Count))
 			if err = os.Remove(g.FilePath); err != nil {
 				return nil, err
 			}
+		} else {
+			newGFiles = append(newGFiles, g)
 		}
 	}
 
-	return gFiles, nil
+	return newGFiles, nil
 }
 
 func getAllJSONInDirectory(dir string) ([]string, error) {
